551504ae0d17e7717564181fefae370c33b17ec1
Shruti Shivasharan <90452196+ShrutiShivasharan@users.noreply.github.com>
2022-08-13 11:26:42 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,284 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+	<title>Covid-19 blog</title>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+
+</head>
+<style id="table_style" type="text/css">
+    body
+    {
+        font-family: Arial;
+        font-size: 10pt;
+    }
+    table
+    {
+        border: 1px solid #ccc;
+        border-collapse: collapse;
+    }
+    table th
+    {
+        background-color: #F7F7F7;
+        color: #333;
+        font-weight: bold;
+    }
+    table th, table td
+    {
+        padding: 5px;
+        border: 1px solid #ccc;
+    }
+    input{
+	width: 120px;
+}
+.box2{
+	margin-top: -45px;
+}
+</style>
+
+<body>
+<div class="container py-5 my-5" id="dvContents" >
+	
+	<table class="text-center">
+		<tbody>
+			<tr>
+				<td><img src="logo2.jpg" width="50px" height="50px "></td>
+				<td cellpadding = "5" class="mx-5 px-5" colspan = "2" >
+				<h3><b>HARDNESS TEST CERTIFICATE</b></h3>
+				<p>Pune-1403</p>
+			</td>
+			</tr>
+		</tbody>
+		<!-- 2 -->
+		<tbody>
+			<tr>
+				<td colspan = "2" class="text-left">METALLURGICAL TEST REPORT NO: <input type="" name=""></td>
+				<td class="text-left">DATE: <input type="" name=""></td><br>
+			</tr>
+			<tr>
+			<td colspan = "3" class="text-left">BATCH NO: <input type="" name=""></td>
+		</tr>
+		</tbody>
+		<!-- 3 -->
+		<tbody>
+			<tr>
+         	<td>CUSTOMER NAME:</td><br>
+         	<td><input type="" name=""></td>
+         	<td width = "400">OUR DC NO.: <input type="" name=""> DATE: <input type="" name=""><br>
+         		CUS DC NO.: <input type="" name=""> DATE: <input type="" name=""><br>
+         		P.O.NO.: <input type="" name=""> DATE: <input type="" name="">
+         	</td>
+         </tr>
+         <tr>
+         	<td>PART NAME:</td><br>
+         	<td><input type="" name=""></td>
+         	<td width = "400" class="text-left">QUANTITY: <input type="" name=""><br>
+         	<p class="text-center">CASH DEPTH (MM)</p>
+         	</td>
+         </tr>
+         <tr>
+         	<td>PART NO.:</td><br>
+         	<td><input type="" name=""></td>
+         	<td>
+         	<table>
+         			<td class="px-5">Specified</td>
+         			<td class="px-5">Cutoff</td>
+         			<td class="px-5">Actual</td>
+         	</table>
+         </td>
+         </tr>
+		</tbody>
+		<!-- 4 -->
+		<tbody>
+		<tr>
+        <td>MATERAIL SPECIFICATION :</td><br>
+        <td><input type="" name=""></td>
+        <td>
+        <table>
+         			<td class="px-5 text-left">--TO<br>--mm</td>
+         			<td class="px-5">55 HV</td>
+         			<td class="px-5"><input type="" name=""></td>
+        </table>
+        </td>
+    </tr>
+    </tbody>
+
+    <!-- 5 -->
+    <tbody>
+    	<tr>
+    		<td>MATERAIL SPECIFICATION :</td><br>
+        <td><input type="" name=""></td>
+        <td>SURFACE HARDNESS</td>
+    	</tr>
+    </tbody>
+
+    <!-- 6 -->
+    <tbody>
+    	<tr>
+    		<td colspan = "2" >Micro Hardness Transvere </td><br>
+       	<td>
+         	<table>
+         			<td class="px-5" width="100%">Specified </td>
+         			<td class="px-5"  width="100%" >Actual</td>
+         	</table>
+         </td>
+    	</tr>
+    </tbody>
+
+    <!-- 7 -->
+
+<tbody>
+	<td colspan="3">
+	<table width="100%" class="box2">
+		<tr>
+			<td>Dist(mm)</td>
+			<td>HV1 </td>
+			<td> Dist(mm)</td>
+			<td> HV1</td>
+			<td rowspan="6">specified2<input type="text"></td>
+			<td>HRC</td>
+			<td>Qty(nos)</td>
+		</tr>
+	
+		<tr>
+			<td>0.1</td>
+			<td><input type="text"></td>
+			<td>0.2</td>
+			<td><input type="text"></td>
+			<td><input type="text" ></td>
+			<td><input type="text"></td>
+		</tr>
+
+		<tr>
+			<td>0.4</td> 
+			<td><input type="text"></td>
+			<td>0.6</td>
+			<td><input type="text"></td>
+			<td><input type="text"></td>
+			<td><input type="text"></td>
+		</tr>
+
+		<tr>
+			<td>0.8</td>
+			<td><input type="text"></td>
+			<td>1</td>
+			<td><input type="text"></td>
+			<td> <input type="text"></td>
+			<td><input type="text"></td>
+		</tr> 
+
+		<tr>
+			<td>1.2</td>
+			<td><input type="text"></td>
+			<td>1.4</td>
+			<td><input type="text" ></td>
+			<td><input type="text" ></td>
+			<td><input type="text"></td>
+		</tr>
+		
+		<tr>
+			<td>1.6</td>
+			<td><input type="text"></td>
+			<td>1.8</td>
+			<td><input type="text"></td>
+			<td><input type="text"></td>
+			<td><input type="text"></td>
+		</tr>
+
+		<tr>
+			<td>2</td>
+			<td><input type="text"></td>
+			<td>2.2</td>
+			<td><input type="text"></td>
+			<td colspan="3"><b>CORE HARDNESS</b></td>			
+		</tr>
+
+		<tr>
+			<td>2.4</td>
+			<td><input type="text"></td>
+			<td>2.6</td>
+			<td><input type="text"></td>
+			<td colspan="2">Specified 3 </td>
+			<td>Actual 3</td>
+		</tr>
+
+		<tr>
+			<td>2.8</td>
+			<td><input type="text"></td>
+			<td>3</td>
+			<td><input type="text"></td>
+			<td colspan="2"><input type="text"></td>
+			<td><input type="text"></td>
+		</tr>
+
+		<tr>
+			<td>3.2</td>
+			<td><input type="text"></td>
+			<td>3.4</td>
+			<td><input type="text"></td>
+			<td colspan="2"><span><mark><b>LOT IS<input type="text"></b></mark></span></td>
+			<td  rowspan="3"><img src="sign.jpg" width="50px" height="50px "></td>
+		</tr>
+
+		<tr>
+			<td>3.6</td>
+			<td><input type="text"></td>
+			<td>3.8</td>
+			<td><input type="text"></td>
+		</tr>
+		<tr>
+			<td colspan="6">Remark<span style="padding:30px"><input type="text" ></td></span><br><br></td>
+		</tr>
+</table>
+</td>
+</tbody>
+
+<!-- 8 -->
+<tbody>
+	<tr>
+		<td colspan="3" class="mt-5 pt-5 mb-0 pb-0"><span class="text-center mx-auto">AUTHORISED SIGNATURE</span><p class="text-left">CHECKED BY</p> </td>
+</tbody>
+
+	</table>
+</div>
+
+<input type="button" onclick="PrintTable();" value="Print" class="btn btn-primary" />
+
+<script type="text/javascript">
+    function PrintTable() {
+        var printWindow = window.open('', '', 'height=200,width=400');
+        printWindow.document.write('<html><head><title>Table Contents</title>');
+ 
+        //Print the Table CSS.
+        var table_style = document.getElementById("table_style").innerHTML;
+        printWindow.document.write('<style type = "text/css">');
+        printWindow.document.write(table_style);
+        printWindow.document.write('</style>');
+        printWindow.document.write('</head>');
+ 
+        //Print the DIV contents i.e. the HTML Table.
+        printWindow.document.write('<body>');
+        var divContents = document.getElementById("dvContents").innerHTML;
+        printWindow.document.write(divContents);
+        printWindow.document.write('</body>');
+ 
+        printWindow.document.write('</html>');
+        printWindow.document.close();
+        printWindow.print();
+    }
+</script>
+
+</body>
+</html>
+
